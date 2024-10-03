ced7ca448dd0fecb168306092473aac8df76013d
Dustin Lodge <lodge.dustin@outlook.com>
2024-10-03 15:09:05 -0600

    updated server.js

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-/*
+
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
-*/
+
 
 // week 3
 // Inventory routes
